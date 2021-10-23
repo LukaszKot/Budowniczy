c56a3f7f309838007faba2888b81af07a3cf58bc
Łukasz Kot <lukaszkot@ukaszs-MacBook-Pro.local>
2021-10-23 13:49:41 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Budowniczy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
